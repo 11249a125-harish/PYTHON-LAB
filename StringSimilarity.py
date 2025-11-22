0cd0ad4554247bd30d74010aaa7c08bde40a744d
K HARISH <11249a125@kanchiuniv.ac.in>
2025-11-22 21:23:24 +0530

    Update StringSimilarity.py

--- StringSimilarity.py
+++ StringSimilarity.py
@@ -1,4 +1,4 @@
-➡️ Aim:
+'''➡️ Aim:
 To write a Python program to find the similarity percentage between two given strings using SequenceMatcher.
 
 ➡️ Algorithm:
@@ -10,7 +10,7 @@ To write a Python program to find the similarity percentage between two given st
 • Display the result.  
 • Stop the program.  
 
-➡️ Program:
+➡️ Program:'''
 from difflib import SequenceMatcher
 
 s1 = input("Enter first string: ")
@@ -19,5 +19,5 @@ s2 = input("Enter second string: ")
 similarity = SequenceMatcher(None, s1, s2).ratio()
 print(f"String Similarity: {similarity * 100:.2f}%")
 
-➡️ Result:
-Thus the Python program that finds the similarity percentage between two given strings is executed successfully.
+#➡️ Result:
+#Thus the Python program that finds the similarity percentage between two given strings is executed successfully.
